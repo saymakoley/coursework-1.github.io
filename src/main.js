5cd9b58ad55842dc306a72c905fb04fb4f56b6a6
saymakoley <97885143+saymakoley@users.noreply.github.com>
2023-03-17 18:52:25 +0000

    install tailwinc

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './assets/index.css'
 import './registerServiceWorker'
+import './assets/tailwind.css'
 
 Vue.config.productionTip = false
 
